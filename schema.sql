cb3426c81a14294607610dfeb5b89bba8b925b42
NolanHP <nolan@peloquin.us>
2026-09-17 21:57:10 -0700

    Add databases for workouts and exercises

---
+++ schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE workouts(
+    id INTEGER PRIMARY KEY,
+    date TEXT NOT NULL
+);
+
+CREATE TABLE exercises(
+    id INTEGER PRIMARY KEY,
+    workouts_id INTEGER NOT NULL REFERENCES workouts(id),
+    name TEXT NOT NULL,
+    weight INTEGER NOT NULL,
+    reps INTEGER NOT NULL,
+    sets INTEGER NOT NULL
+);
