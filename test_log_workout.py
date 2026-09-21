b7349d9a196793bf626517add6a94c05bb053df8
NolanHP <nolan@peloquin.us>
2026-09-20 17:20:49 -0700

    Add test for insert_exercise

--- test_log_workout.py
+++ test_log_workout.py
@@ -10,3 +10,11 @@ create_tables(db)
 def test_insert_workout():
     result = insert_workout(db, "2008-03-04")
     assert result == 1
+
+
+def test_insert_exercise():
+    workout_id_test = insert_workout(db, "2009-03-03")
+    result = insert_exercise(db, workout_id_test, "Pushups", 100, 20, 3)
+    cursor.execute("SELECT * FROM exercises WHERE id = (?);", (result,))
+    row = cursor.fetchone()
+    assert row == (result, workout_id_test, "Pushups", 100, 20, 3)
